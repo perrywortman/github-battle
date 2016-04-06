484393a51605874ce865d91fc295e2585311b7bd
W. Perry Wortman <pwortman@visioncreativegroup>
2016-04-06 16:08:31 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Github Battle
+
+A work in progress, docs coming soon.
---
+++ app/index.js
@@ -0,0 +1,16 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var routes = require('./config/routes')
+
+/*
+ * Focused
+ * Independent
+ * Reusable
+ * Small
+ * Testable
+ */
+
+ReactDOM.render(
+  routes,
+  document.getElementById('app')
+)
