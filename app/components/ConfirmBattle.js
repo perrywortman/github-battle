3f9868711f3e304e6914f00603fd3cafa9f39f19
W. Perry Wortman <pwortman@visioncreativegroup>
2016-04-14 09:37:00 -0400

    add results path and component/container

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -11,16 +11,16 @@ function ConfirmBattle(props){
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className="col-sm-8 col-sm-offset-2">
-          <UserDetailsWrapper header='Player 1'>
+          <UserDetailsWrapper header='Player One'>
             <UserDetails info={props.playersInfo[0]} />
           </UserDetailsWrapper>
-          <UserDetailsWrapper header='Player 2'>
+          <UserDetailsWrapper header='Player Two'>
             <UserDetails info={props.playersInfo[1]} />
           </UserDetailsWrapper>
         </div>
         <div className="col-sm-8 col-sm-offset-2">
           <div className="col-sm-12" style={styles.space}>
-            <button type="button" className="btn btn-lg btn-success" onCLick={this.onInitiateBattle}>
+            <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
               Initiate Battle!
             </button>
           </div>
