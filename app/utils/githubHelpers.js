e1bc55f5f9811f3c5fa837ed032e6d72d3725ebd
W. Perry Wortman <perry.wortman@dashlane.com>
2018-03-25 23:37:39 -0400

    es6 syntax, format with prettier

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,62 +1,65 @@
-import axios from 'axios'
-const apiKeys = require('../../apiKeys')
-const id = apiKeys.CLIENT_ID
-const sec = apiKeys.SECRET_ID
-const param = '?client_id=' + id + '&client_secret=' + sec
+import axios from 'axios';
+const apiKeys = require('../../apiKeys');
+const id = apiKeys.CLIENT_ID;
+const sec = apiKeys.SECRET_ID;
+const param = '?client_id=' + id + '&client_secret=' + sec;
 
-function getUserInfo(username) {
-  return axios.get('https://api.github.com/users/' + username + param)
-}
+const getUserInfo = username => {
+  return axios.get('https://api.github.com/users/' + username + param);
+};
 
-function getRepos(username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100')
-}
+const getRepos = username => {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+};
 
-function getTotalStars(repos) {
+const getTotalStars = repos => {
   return repos.data.reduce((prev, current) => {
-    return prev + current.stargazers_count
-  }, 0)
-}
+    return prev + current.stargazers_count;
+  }, 0);
+};
 
-function getPlayersData(player) {
+const getPlayersData = player => {
   return getRepos(player.login)
-  .then(getTotalStars)
-  .then(totalStars => {
-    return {
-      followers: player.followers,
-      totalStars: totalStars
-    }
-  })
-}
+    .then(getTotalStars)
+    .then(totalStars => {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      };
+    });
+};
 
-function calculateScores(players) {
-  return [
-    players[0].followers * 3 + players[0].totalStars,
-    players[1].followers * 3 + players[1].totalStars
-  ]
-}
+const calculateScores = players => {
+  return [players[0].followers * 3 + players[0].totalStars, players[1].followers * 3 + players[1].totalStars];
+};
 
-var githubHelpers = {
+const githubHelpers = {
   getPlayersInfo: players => {
-    return axios.all(players.map(username => {
-      return getUserInfo(username)
-    })).then(info => {
-      return info.map(user => {
-        return user.data
+    return axios
+      .all(
+        players.map(username => {
+          return getUserInfo(username);
+        })
+      )
+      .then(info => {
+        return info.map(user => {
+          return user.data;
+        });
       })
-    }).catch(err => {
-      console.warn('Error in getPlayersInfo', err)
-    })
+      .catch(err => {
+        console.warn('Error in getPlayersInfo', err);
+      });
   },
   battle: players => {
-    var playerOneData = getPlayersData(players[0])
-    var playerTwoData = getPlayersData(players[1])
-    return axios.all([playerOneData, playerTwoData])
-    .then(calculateScores)
-    .catch(err => {
-      console.warn('Error in battle: ', err)
-    })
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+    return axios
+      .all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(err => {
+        console.warn('Error in battle: ', err);
+      });
   }
-}
+};
 
-export default githubHelpers
+export default githubHelpers;
